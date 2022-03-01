e04b84795c42c48e51350b92d37608fbeb9dd404
GitPit <rifflifter@gmail.com>
2022-03-01 15:32:18 -0500

    added contact section divs

--- index.php
+++ index.php
@@ -170,6 +170,17 @@
 </section>
 <!-- end category 3 section -->
 
+<!-- begin contact section -->
+
+<section class="my-4">
+    <div class="py-4">
+        <h2 class="text-center">Contact Us</h2>
+    </div>
+
+</section>
+
+<!-- end contact section -->
+
 
 <script> src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
