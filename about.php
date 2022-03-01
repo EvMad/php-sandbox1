c020ef6e3b433ba4606ce285dbb2c6e12c9b5625
GitPit <rifflifter@gmail.com>
2022-03-01 15:55:34 -0500

    wrote queries, but need to create database

--- about.php
+++ about.php
@@ -10,4 +10,15 @@ else {
     echo "Connection Failed";
 }
 
+mysqli_select_db($con, '');
+
+$name = $_POST['name'];
+$email = $_POST['email'];
+$number = $_POST['number'];
+
+$query = "INSERT INTO users (name, email, number) VALUES ('$name', '$email', '$number')";
+
+mysqli_query($con, $query);
+header('location:index.php');
+
 ?>
