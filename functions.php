efcf54c30ca937f2a551d314a0661f8dbeda9d7e
GitPit <rifflifter@gmail.com>
2022-03-07 16:02:06 -0500

    wrote switch case for calculator add and subtract

--- functions.php
+++ functions.php
@@ -4,6 +4,19 @@
 
 function myCalculator($num01, $oper, $num02) {
     $sum;
+
+    switch ($oper) {
+        case "add":
+            $sum = $num01 + $num02;
+            break;
+        case "sub":
+            $sum = $num01 - $num02;
+            break;
+        default:
+        $sum = "There was an error calculating the dogs and cats!";
+        break;
+    }
+
 }
 
 ?>
