d25fa0f356103d3a3f9c0a5889451316264e9261
GitPit <rifflifter@gmail.com>
2022-03-07 15:48:58 -0500

    added form div for calculator section

--- index.php
+++ index.php
@@ -221,6 +221,12 @@
 </a>
 <!-- end  about section -->
 
+<!-- calculator -->
+
+<form action="functions.php" method="get">
+
+</form>
+
 
 <script> src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"</script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
