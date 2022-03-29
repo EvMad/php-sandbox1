6514e18fb101ad2836ac450c6f7dcf7eece59cd8
GitPit <rifflifter@gmail.com>
2022-03-28 22:16:36 -0400

    port: 8000 / keepalive / open / bin / router

--- runphp.js
+++ runphp.js
@@ -1,4 +1,10 @@
 import phpServer from 'php-server';
 
-const server = await phpServer();
+const server = await phpServer({
+    port: 8000,
+    keepalive: false,
+    open: false,
+    bin: 'php',
+    router: __dirname + '/server.php'
+});
 console.log(`PHP server running at ${server.url}`);
